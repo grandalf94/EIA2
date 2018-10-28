ab29134d7411ed76c3d3723ac4bd10bc12abaf9e
Aang <Aang@DESKTOP-OJ4PKMG.fritz.box>
2018-10-28 10:40:01 +0100

    uno Simpler

---
+++ Aufgabe_2/unoSimpler.js
@@ -0,0 +1,40 @@
+/**-- Aufgabe: Aufgabe 1
+Name: Joel Stark
+Matrikel: 259546
+Datum: 28.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.*/
+var unoSimple;
+(function (unoSimple) {
+    let colors = ["red", "blue", "green", "yellow"];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
+    let cards = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
+    for (let i = 0; i < colors.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            for (let k = 0; k < 2; k++) {
+                let card = { color: colors[i], value: values[j] };
+                cards.push(card);
+            }
+        }
+    }
+    console.log(cards);
+    let handcards = [];
+    function Eingabe() {
+        var kartenanzahl = prompt("Kartenanzahl");
+        let n = parseInt(kartenanzahl);
+        for (let anz = n; anz > 0; anz--) {
+            let r = Math.floor(Math.random() * (cards.length - 1));
+            handcards.push(cards[r]);
+            cards.splice(r, 1);
+        }
+        for (let b = 0; b < handcards.length; b++) {
+            let div = document.createElement("div");
+            document.getElementById("Handkarten").appendChild(div);
+            div.innerHTML = handcards[b].value;
+            div.classList.add("Handkarten");
+            div.classList.add(handcards[b].color);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", Eingabe);
+})(unoSimple || (unoSimple = {}));
+//# sourceMappingURL=unoSimpler.js.map
---
+++ Aufgabe_2/unoSimpler.ts
@@ -0,0 +1,49 @@
+/**-- Aufgabe: Aufgabe 1
+Name: Joel Stark
+Matrikel: 259546
+Datum: 28.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+Er wurde nicht kopiert und auch nicht diktiert.*/
+namespace unoSimple{
+    interface Unocard {
+        color: string;
+        value: string;
+    }
+
+    let colors: string[] = ["red", "blue", "green", "yellow"];
+    let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
+    let cards: Unocard[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
+
+    for (let i: number = 0; i < colors.length; i++) {
+        for (let j: number = 0; j < values.length; j++) {
+            for (let k: number = 0; k < 2; k++) {
+                let card: Unocard = { color: colors[i], value: values[j] };
+                cards.push(card);
+            }
+        }
+    }
+    console.log(cards);
+
+    let handcards: Unocard[] = [];
+
+    function Eingabe(): void {
+        var kartenanzahl: string = prompt("Kartenanzahl");
+        let n: number = parseInt(kartenanzahl);
+
+        for (let anz: number = n; anz > 0; anz--) {
+            let r: number = Math.floor(Math.random() * (cards.length - 1));
+            handcards.push(cards[r]);
+            cards.splice(r, 1);
+        }
+
+        for (let b: number = 0; b < handcards.length; b++) {
+            let div: HTMLElement = document.createElement("div");
+            document.getElementById("Handkarten").appendChild(div);
+            div.innerHTML = handcards[b].value;
+            div.classList.add("Handkarten");
+            div.classList.add(handcards[b].color);
+        }
+        
+    }
+    document.addEventListener("DOMContentLoaded", Eingabe);
+}
