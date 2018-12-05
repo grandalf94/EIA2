76c3afe03f7a60d77703103b1980c57ec37be2ad
Aang <Aang@DESKTOP-OJ4PKMG.fritz.box>
2018-12-05 20:35:59 +0100

    dad

--- Aufgabe_7/Server/Server.js
+++ Aufgabe_7/Server/Server.js
@@ -1,51 +1,31 @@
 "use strict";
 const Http = require("http");
 const Url = require("url");
-var A7;
-(function (A7) {
+var L06_SendData;
+(function (L06_SendData) {
     console.log("Starting server");
     let port = process.env.PORT;
     if (port == undefined)
         port = 8100;
-    let HTMLArray = [];
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
-    server.listen(port);
     function handleListen() {
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log(_request.url);
-        console.log("request");
+        console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        if (_request.url != "/favicon.ico") {
-            let url = Url.parse(_request.url).search.substr(1);
-            let HTML = "<br>";
-            for (let i = 0; i < url.length; i++) {
-                if (url[i] == "&") {
-                    HTMLArray.push(HTML);
-                    HTML = "<br>";
-                }
-                else {
-                    if (HTML == "<br>Text") {
-                        HTML = "<br>Adresse";
-                    }
-                    if (HTML == "<br>Pattern") {
-                        HTML = "<br>Hausnummer";
-                    }
-                    HTML += url[i];
-                    console.log(HTML);
-                }
-            }
-            HTMLArray.push(HTML);
-            for (let i = 0; i < HTMLArray.length; i++) {
-                _response.write(HTMLArray[i]);
-            }
-            console.log(HTMLArray);
+        console.log(_request.url);
+        let url = Url.parse(_request.url, true).query;
+        console.log(url);
+        for (let key in url) {
+            console.log(url[key]);
+            console.log(key);
+            _response.write(key + " = " + url[key] + "<br>");
         }
         _response.end();
     }
-})(A7 || (A7 = {}));
+})(L06_SendData || (L06_SendData = {}));
 //# sourceMappingURL=Server.js.map
--- Aufgabe_7/Server/Server.ts
+++ Aufgabe_7/Server/Server.ts
@@ -1,59 +1,48 @@
 import * as Http from "http"; 
 import * as Url from "url";
 
-namespace A7 {
-    console.log("Starting server");
+namespace L06_SendData {
+    interface Product {
+        [key: string]: number;
+    }
+    console.log("Starting server"); 
     let port: number = process.env.PORT; 
-    if (port == undefined)
+    if (port == undefined) 
         port = 8100;
-    
-    
-    let HTMLArray: string[] = [];
-    
-    let server: Http.Server = Http.createServer(); 
+
+    let server: Http.Server = Http.createServer();  
     server.addListener("request", handleRequest); 
     server.addListener("listening", handleListen); 
-    server.listen(port); 
 
     function handleListen(): void {
-        console.log("Listening");
+        console.log("Listening"); 
     }
+
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 
-        console.log(_request.url); 
-        console.log("request");
-        
+        console.log("I hear voices!"); 
+
         _response.setHeader("content-type", "text/html; charset=utf-8"); 
         _response.setHeader("Access-Control-Allow-Origin", "*"); 
-        if (_request.url != "/favicon.ico") {
-            let url: string = Url.parse(_request.url).search.substr(1);
-            let HTML: string = "<br>";
-            for (let i: number = 0; i < url.length; i++) {
-                if (url[i] == "&") {
-                    HTMLArray.push(HTML);
-                    HTML = "<br>";
-                }
-                else {
-                    if (HTML == "<br>Text") {
-                        HTML = "<br>Adresse"
-                        }
-                    if (HTML == "<br>Pattern") {
-                        HTML = "<br>Hausnummer"
-                        }
-                    HTML += url[i];
-                    console.log(HTML);
-                }
-                
-                
-            }
-            HTMLArray.push(HTML);
-            
-            for (let i: number = 0 ; i < HTMLArray.length ; i++) {
-                _response.write(HTMLArray[i]);
-                }
-            console.log(HTMLArray);
+
+     
+        console.log(_request.url);
+
+        let url: Product = Url.parse(_request.url, true).query;
+        console.log(url);
+
+
+        for (let key in url) {
           
+            console.log(url[key]);
+            console.log(key);
+            
+            _response.write(key + " = " + url[key] + "<br>");
         }
 
-        _response.end(); 
-    } 
+
+
+
+        _response.end();    
+    }
+
 }
