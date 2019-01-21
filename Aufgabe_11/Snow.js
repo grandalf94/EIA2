ccb34fe4ecdab4abccb0f8d03b4ce2c203fe3896
Aang <Aang@DESKTOP-OJ4PKMG.fritz.box>
2019-01-22 00:49:57 +0100

    da

--- Aufgabe_11/Snow.js
+++ Aufgabe_11/Snow.js
@@ -1,1 +1,31 @@
+var A11_Rodelbahn;
+(function (A11_Rodelbahn) {
+    class Snow extends A11_Rodelbahn.DrawObjects {
+        constructor() {
+            super();
+            this.xPos = Math.random() * 700;
+            this.yPos = Math.random() * 1100;
+        }
+        move() {
+            this.yDir = 2;
+            this.xDir = Math.random() * 4 - 2;
+            this.xPos += this.xDir;
+            this.yPos += this.yDir;
+            if (this.yPos > 1100) {
+                this.yPos = 0;
+            }
+            this.draw();
+        }
+        draw() {
+            A11_Rodelbahn.crc2.beginPath();
+            A11_Rodelbahn.crc2.arc(this.xPos, this.yPos, 3, 0, 2 * Math.PI, false);
+            A11_Rodelbahn.crc2.fillStyle = "#F5FEFF";
+            A11_Rodelbahn.crc2.fill();
+            A11_Rodelbahn.crc2.lineWidth = .2;
+            A11_Rodelbahn.crc2.strokeStyle = "#D2EEF2";
+            A11_Rodelbahn.crc2.stroke();
+        }
+    }
+    A11_Rodelbahn.Snow = Snow;
+})(A11_Rodelbahn || (A11_Rodelbahn = {}));
 //# sourceMappingURL=Snow.js.map
--- Aufgabe_11/Trees.js
+++ Aufgabe_11/Trees.js
@@ -1,1 +1,28 @@
+var A11_Rodelbahn;
+(function (A11_Rodelbahn) {
+    class Trees extends A11_Rodelbahn.DrawObjects {
+        constructor() {
+            super();
+            this.xPos = 40 + Math.random() * 200;
+            this.yPos = 800 + Math.random() * 200;
+        }
+        draw() {
+            A11_Rodelbahn.crc2.beginPath();
+            A11_Rodelbahn.crc2.moveTo(this.xPos - 10, this.yPos + 70);
+            A11_Rodelbahn.crc2.lineTo(this.xPos + 10, this.yPos + 70);
+            A11_Rodelbahn.crc2.lineTo(this.xPos + 7, this.yPos - 0);
+            A11_Rodelbahn.crc2.closePath();
+            A11_Rodelbahn.crc2.fillStyle = "#612A05";
+            A11_Rodelbahn.crc2.fill();
+            A11_Rodelbahn.crc2.beginPath();
+            A11_Rodelbahn.crc2.moveTo(this.xPos - 30, this.yPos + 50);
+            A11_Rodelbahn.crc2.lineTo(this.xPos, this.yPos - 20);
+            A11_Rodelbahn.crc2.lineTo(this.xPos + 30, this.yPos + 50);
+            A11_Rodelbahn.crc2.closePath();
+            A11_Rodelbahn.crc2.fillStyle = "#018A0E";
+            A11_Rodelbahn.crc2.fill();
+        }
+    }
+    A11_Rodelbahn.Trees = Trees;
+})(A11_Rodelbahn || (A11_Rodelbahn = {}));
 //# sourceMappingURL=Trees.js.map
